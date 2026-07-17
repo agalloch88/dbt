87c2f10b0ae9b585caa00f6fa896da2df669943c
Ryan Kirsch <ryankirsch88@gmail.com>
2026-07-17 19:57:18 +0000

    ephemeral orders model

---
+++ models/demo/clean_orders.sql
@@ -0,0 +1,20 @@
+{{
+    config (
+        materialized = 'ephemeral'
+    )
+}}
+
+WITH base_orders AS (
+    SELECT
+        ORDER_ID,
+        ORDER_DATE,
+        CUSTOMER_ID,
+        CASE 
+            WHEN CUSTOMER_NAME IS NULL THEN 'NA' 
+            ELSE upper(CUSTOMER_NAME) 
+        END AS CUSTOMER_NAME,
+        CREATED_AT
+    FROM {{source('orders','BASE_ORDERS')}}
+    WHERE ORDER_DATE IS NOT NULL
+)
+SELECT * FROM BASE_ORDERS
