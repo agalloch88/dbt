edb98d1603c554fd87570814c9c94bbf0721c440
Ryan Kirsch <ryankirsch88@gmail.com>
2026-08-29 15:17:52 +0000

    set up batch scd2 macro,  sql, yml. files

---
+++ models/batch_scd2/product_raw.sql
@@ -0,0 +1,33 @@
+DBT product_raw.sql Model Code
+{{
+config
+(
+    materialized = "incremental",
+    incremental_strategy = "append",
+    pre_hook = "{{ copy_into_snowflake('PRODUCT_CP') }}",
+    schema = "RAW"
+)
+}}
+
+ 
+
+WITH product_cp AS
+(
+    SELECT 
+    SOURCE_DATA:product_id AS PRODUCT_ID,
+    SOURCE_DATA AS SOURCE_DATA,
+    SOURCE_FILENAME AS SOURCE_FILENAME,
+    CURRENT_TIMESTAMP(6) AS INSERT_DTS
+    FROM {{source('product','PRODUCT_CP')}}
+
+    {% if is_incremental() %}
+    WHERE INSERT_DTS > (SELECT MAX(INSERT_DTS) FROM {{this}})
+    {% endif %}
+)
+
+SELECT
+CAST(PRODUCT_ID AS INT) AS PRODUCT_ID,
+CAST(SOURCE_DATA AS VARIANT) AS SOURCE_DATA,
+CAST(SOURCE_FILENAME AS STRING) AS SOURCE_FILENAME,
+CAST(INSERT_DTS AS TIMESTAMP(6)) AS INSERT_DTS
+FROM product_cp
